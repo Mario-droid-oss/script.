2e87f081f8bc6c44030d32a6639e2d0754cc3224
Mario-droid-oss <severinmario71@gmail.com>
2025-06-27 18:22:06 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Scripting.APIUpdating;
+
+public class NPC : MonoBehaviour
+{
+    //NPC Healh
+    public int healh = 5;
+
+    //NPC Level
+    public int level = 1;
+    //NPC Seead
+    public float speed = 1.2f;
+
+    // Update is called before the first frame update void start
+    void Start()
+    {
+        
+
+    }
+
+    private void Update()
+    {
+        
+ 
+     
+    }
+
+}
+
