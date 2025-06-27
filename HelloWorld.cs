6652fe2894c3b5131d253620506ee4b3cc6e12e4
Mario-droid-oss <severinmario71@gmail.com>
2025-06-27 17:27:13 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HelloWorld : MonoBehaviour
+{     
+  // Update is called before the first frame update void start
+    void Start()
+    {
+        print("Hello,  World");
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
+
+   
